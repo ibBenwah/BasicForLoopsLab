12faca9a3000f87c0d647bd419c060fab6bee9c5
Garret <Garret@LAPTOP-61247CEE>
2022-07-03 22:45:14 -0400

    Add project files.

---
+++ BasicForLoopsLab/Program.cs
@@ -0,0 +1,22 @@
+﻿bool isAgain = true;
+do
+{
+    Console.Write("Enter a number: ");
+    int n, sum, i;
+    sum = 0;
+    n = int.Parse(Console.ReadLine());
+    for (i = 0; i <= n; i++)
+    {
+        sum += i;
+    } Console.WriteLine("The sum of the numbers is: " + sum);
+    Console.WriteLine("do you want to enter another number? y/n ");
+    if (Console.ReadLine() == "y")
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!!");
+        break;
+    }
+} while (true);
